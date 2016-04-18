8324b92948bb67eb6fb53e1a89a90f8f41bc3878
Chris Rabuse <rab233@live.com>
2016-04-17 20:41:56 -0500

    Complete pro mode

--- client/client.js
+++ client/client.js
@@ -1,5 +1,67 @@
 var app = angular.module('projectTrackerApp', []);
 
 app.controller('ticketController', ['$http', function($http) {
-  
+  var vm = this;
+  vm.tickets = [];
+  vm.openTickets = 0;
+
+  // Get all tickets
+  vm.get = function() {
+    $http.get('/ticket').then(function(response) {
+      console.log(response.data);
+      vm.tickets = response.data;
+      vm.openTickets = vm.tickets.length;
+    });
+  };
+
+  // Send a ticket to be created
+  vm.create = function() {
+    var ticket = {
+      name: vm.name,
+      type: vm.type,
+      priority: vm.priority,
+      description: vm.description,
+      assignee: vm.assignee,
+      reporter: vm.reporter
+    }
+    $http.post('/ticket/create', ticket).then(function() {
+      vm.get();
+    });
+  };
+
+  // Make tickets editable
+  vm.makeEditable = function(ticket) {
+    ticket.editable = true;
+    return ticket;
+  };
+
+  vm.edit = function(ticket) {
+    ticket.editable = false;
+
+    var api = '/ticket/edit/' + ticket._id;
+    $http.post(api, ticket).then(function(response) {
+      console.log(response);
+    });
+
+    return ticket;
+  };
+
+  // Delete tickets
+  vm.remove = function(id) {
+    var api = '/ticket/remove/' + id;
+
+    $http.delete(api).then(function() {
+      console.log('Ticket removed succesfully');
+      vm.get();
+    });
+  };
+
+  // Check input
+  vm.checkInput = function() {
+    if (vm.name && vm.type && vm.priority && vm.description && vm.assignee && vm.reporter) {
+      vm.create();
+    }
+  };
+
+  vm.get();
 }]);
--- server/routers/ticket.js
+++ server/routers/ticket.js
@@ -6,23 +6,96 @@ var Ticket = require('../../models/ticket');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  // Get all
-  res.sendStatus(200);
+  // Get all tickets from the database
+  Ticket.find({}, function(err, tickets) {
+    if(err) {
+      console.log('Getting tickets failed:', err);
+      res.sendStatus(500);
+    } else {
+      res.send(tickets);
+    }
+  });
 });
 
 router.post('/create', function(req, res) {
   // create new ticket
-  res.sendStatus(200);
+  var ticket = new Ticket({
+    name: req.body.name,
+    type: req.body.type,
+    priority: req.body.priority,
+    description: req.body.description,
+    assignee: req.body.assignee,
+    reporter: req.body.reporter,
+    created: new Date(),
+    updated: new Date()
+  });
+
+  ticket.save(function(err) {
+    if(err) {
+      console.log('Creating ticket failed:', err);
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 });
 
 router.post('/edit/:id?', function(req, res) {
-  // Find ticket by ID and edit it
-  res.sendStatus(200);
+  var id = req.params.id;
+
+  console.log('Edit route accessed');
+
+  // Check if the id parameter is included in the request
+  if(id) {
+    var ticket = new Ticket({
+      _id: req.body._id,
+      name: req.body.name,
+      type: req.body.type,
+      priority: req.body.priority,
+      description: req.body.description,
+      assignee: req.body.assignee,
+      reporter: req.body.reporter,
+      created: req.body.created,
+      updated: new Date()
+    });
+
+    // Find the ticket by ID and update
+    Ticket.findByIdAndUpdate(id, ticket, function(err) {
+      if(err) {
+        console.log('Editing ticket failed:', err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 router.delete('/remove/:id?', function(req, res) {
+  var id = req.params.id;
+
   // Find ticket by id and remove it from the database
-  res.sendStatus(200);
+  if(id) {
+    Ticket.find({_id: id}).remove(function(err){
+      if(err) {
+        console.log('Removing ticket by ID failed:', err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  } else {
+    Ticket.find({}).remove(function(err) {
+      if(err) {
+        console.log('Removing all tickets failed:', err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  }
 });
 
 module.exports = router;
